7780c54571ff860b4aea76043470274fbcda0bb2
Yago Riveiro <yago.riveiro@gmail.com>
2016-01-31 16:50:36 +0000

    Add getDefaultOptions to BackoffInterface

--- src/BackoffInterface.php
+++ src/BackoffInterface.php
@@ -3,6 +3,7 @@ namespace Yriveiro\Backoff;
 
 interface BackoffInterface
 {
+    public static function getDefaultOptions();
     public function exponential($attempt);
     public function equalJitter($attempt);
     public function fullJitter($attempt);
